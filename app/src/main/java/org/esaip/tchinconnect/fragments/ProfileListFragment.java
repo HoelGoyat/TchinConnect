5a414fb181dd68c9a6f0c6c11c22472ec9bdfa7f
HoelGoyat <hoel.goyat@sfr.fr>
2023-05-10 15:56:21 +0200

    Feat:continuing the layouts Added layouts for contact list and personal profile

---
+++ app/src/main/java/org/esaip/tchinconnect/fragments/ProfileListFragment.java
@@ -0,0 +1,13 @@
+package org.esaip.tchinconnect.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import org.esaip.tchinconnect.R;
+
+public class ProfileListFragment extends Fragment {
+
+    public ProfileListFragment() {
+        super(R.layout.profile_card_list_fragment);
+    }
+
+}
