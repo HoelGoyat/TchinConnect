7228e88bffaf56d54f19367d49c6a8ae8e9291ca
HoelGoyat <hoel.goyat@sfr.fr>
2023-05-10 18:10:52 +0200

    Feat:continuing the layouts Added layouts for settings and new device, WIP

---
+++ app/src/main/java/org/esaip/tchinconnect/fragments/DeviceListFragment.java
@@ -0,0 +1,13 @@
+package org.esaip.tchinconnect.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import org.esaip.tchinconnect.R;
+
+public class DeviceListFragment extends Fragment {
+
+    public DeviceListFragment() {
+        super(R.layout.fragment_device);
+    }
+
+}
